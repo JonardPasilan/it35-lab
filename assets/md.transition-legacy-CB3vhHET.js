482feed8fcde0270e2ee73576455fa9b3905da6c
jonard <jonardpasilan106@gmail.com>
2025-05-13 21:17:21 +0800

    Updates

--- assets/md.transition-legacy-CB3vhHET.js
+++ assets/md.transition-legacy-CB3vhHET.js
@@ -1,4 +1,4 @@
-System.register(["./index-legacy-BTuSnpfa.js"],(function(e,n){"use strict";var t,i;return{setters:[e=>{t=e.o,i=e.n}],execute:function(){
+System.register(["./index-legacy-CNbgWk88.js"],(function(e,n){"use strict";var t,i;return{setters:[e=>{t=e.o,i=e.n}],execute:function(){
 /*!
              * (C) Ionic http://ionicframework.com - MIT License
              */
